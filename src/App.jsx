abbd243bd75c8cef1983bb5df5f2e83705020e5b
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-02-10 22:25:59 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Lenis from "lenis";
 import 'lenis/dist/lenis.css';
 
-import Home from "./components/Home/Home";
-import NavBar from "./components/NavBar/Navbar";
-import About from "./components/About/About";
-import Services from "./components/Services/Services";
-import Footer from "./components/Footer/Footer";
+import AppRoutes from "./components/routes/AppRoutes"; 
 
 function App() {
 
@@ -17,31 +14,28 @@ function App() {
             duration: 1000,
             offset: 100,
         });
+
         const lenis = new Lenis({
             duration: 1.2,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             smoothWheel: true,
         });
+
         function raf(time) {
             lenis.raf(time);
             requestAnimationFrame(raf);
         }
-
         requestAnimationFrame(raf);
+
         return () => {
             lenis.destroy();
         };
+    }, []);
 
-    }, []); 
-
-    return(
-        <>
-        <NavBar/>
-        <Home/>
-        <About/>
-        <Services/>
-        <Footer/>
-        </>
+    return (
+        <BrowserRouter>
+            <AppRoutes /> 
+        </BrowserRouter>
     );
 }
 
